e4fad5498a339911d6ebedd5e5bfd4d1a24e2f25
Ahlam-bi <dreamasahlam@gmail.com>
2025-10-21 16:15:29 +0100

    Ajout du microservice demande-service

---
+++ vintagly-backend/demande-service/src/main/java/com/example/demande_service/DemandeServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.demande_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemandeServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemandeServiceApplication.class, args);
+	}
+
+}
---
+++ vintagly-backend/demande-service/src/test/java/com/example/demande_service/DemandeServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demande_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemandeServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
