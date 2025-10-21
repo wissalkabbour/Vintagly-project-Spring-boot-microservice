1a9fa0da93e68d19326c2278bd705448ada12bdf
safae <labjakhsafae160@gmail.com>
2025-10-21 15:52:22 +0100

    add authentiication service

---
+++ vintagly-backend/authentification_service/src/main/java/com/example/authentification_service/AuthentificationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.authentification_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthentificationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthentificationServiceApplication.class, args);
+	}
+
+}
