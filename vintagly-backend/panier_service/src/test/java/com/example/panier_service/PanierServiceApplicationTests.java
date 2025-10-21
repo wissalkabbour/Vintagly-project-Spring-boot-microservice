55ff9d142e665e1e0e598a33575ac2f11d8275c6
MalikaTajidi <tajidimalika@gmail.com>
2025-10-21 15:49:01 +0100

    backend

---
+++ vintagly-backend/panier_service/src/test/java/com/example/panier_service/PanierServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.panier_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PanierServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
