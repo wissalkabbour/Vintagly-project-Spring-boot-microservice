003cd3ea154b18ab1d938da03b79c37f2c84c6c6
soumaiakeroaunsalah <kerouansalahsoumaia@gmail.com>
2025-10-23 21:58:32 +0000

    add cataloque component

--- vintagly-frontend/src/router/AppRoutes.jsx
+++ vintagly-frontend/src/router/AppRoutes.jsx
@@ -1,13 +1,16 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Register from "../pages/Register";
-
-
+import Catalogue from "../pages/Catalogue";
 const AppRoutes = () => {
   return (
     <BrowserRouter>
       <Routes>
+        {/* 🔹 Route d'inscription */}
         <Route path="/register" element={<Register />} />
+
+        {/* 🔹 Route vers le catalogue */}
+        <Route path="/catalogue" element={<Catalogue />} />
       </Routes>
     </BrowserRouter>
   );
