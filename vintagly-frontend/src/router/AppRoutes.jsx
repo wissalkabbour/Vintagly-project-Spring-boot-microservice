78022d5b2b36073cf7acee50952ec5f62a4a501e
Ahlam-bi <dreamasahlam@gmail.com>
2025-10-23 15:28:39 +0100

    form de demande

--- vintagly-frontend/src/router/AppRoutes.jsx
+++ vintagly-frontend/src/router/AppRoutes.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Register from "../pages/Register";
-
+import { Component as Demande } from "../components/demande";
 
 const AppRoutes = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/register" element={<Register />} />
+        <Route path="/demande" element={<Demande />} />
       </Routes>
     </BrowserRouter>
   );
