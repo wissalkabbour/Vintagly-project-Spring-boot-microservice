1762c6629e9e84f8438e989ef33bacc2a9b35205
safae <labjakhsafae160@gmail.com>
2025-11-18 21:08:47 +0100

    some modifications

--- vintagly-backend/catalogue_service/src/main/java/com/example/controllers/ArticleDemandeController.java
+++ vintagly-backend/catalogue_service/src/main/java/com/example/controllers/ArticleDemandeController.java
@@ -7,7 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/catalogue/articles")
+@RequestMapping("/api/catalogue/articlesD")
 @CrossOrigin(origins = "http://localhost:5173") 
 
 public class ArticleDemandeController {
